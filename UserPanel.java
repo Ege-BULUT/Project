e8b6e459e15af8de4a55fb428bf7e7c20d9c84c1
Bryondr <44525204+Bryondr@users.noreply.github.com>
2019-05-13 17:08:49 +0300

    Create UserPanel.java

---
+++ UserPanel.java
@@ -0,0 +1,16 @@
+public class UserPanel
+{
+    public void Run(UserClass user)
+    {
+        System.out.println("Welcome " + user.getName() + ", please choose an operation;");
+        System.out.println("1. Show Inbox");
+        System.out.println("2. Show Outbox");
+        System.out.println("3. Send Message");
+        System.out.println("4. Share Post");
+        System.out.println("5. Show timeline");
+        System.out.println("6. Show contact list");
+        System.out.println("7. Show Notifications");
+        System.out.println("8. Follow People");
+
+    }
+}
